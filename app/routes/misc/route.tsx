9efd4fa1b3972b4940ae5b4e4ff017833fbaa2a4
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-02-19 00:23:26 -0700

    placeholder routes

---
+++ app/routes/misc/route.tsx
@@ -0,0 +1,25 @@
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  return null;
+}
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Component() {
+  return (
+    <article>
+      <h1>Hi! I'm Abhi :)</h1>
+      <p>
+        I'm a frontend developer, optimist, and community builder. I currently
+        work as the VP of Product at Vercel, where I help teach the Next.js
+        community, an open-source web framework built with React.
+      </p>
+    </article>
+  );
+}
