bb2e3d25f6491765dddd235f4105f41bad9c264a
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-02-22 22:52:16 -0700

    Add basic error boundary for the app

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 
 import styles from "./global.css?url";
@@ -47,5 +49,30 @@ export default function App() {
         rights reserved.
       </footer>
     </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  let status = 500;
+  let message = "An unexpected error occurred.";
+  if (isRouteErrorResponse(error)) {
+    status = error.status;
+    switch (error.status) {
+      case 404:
+        message = "Page Not Found";
+        break;
+    }
+  } else {
+    console.error(error);
+  }
+
+  return (
+    <Layout>
+      <div className="container prose py-8">
+        <h1>{status}</h1>
+        <p>{message}</p>
+      </div>
+    </Layout>
   );
 }
